8eaa2bff4cc89aff7b705fbe72b8bdc9fea0b82e
Panggi Libersa Jasri Akadol <panggilja@gmail.com>
2014-06-13 12:47:50 +0700

    first commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name              "unicorn-init"
+maintainer        "Panggi Libersa Jasri Akadol"
+maintainer_email  "panggi@me.com"
+description       "Unicorn init script"
+version           "0.0.1"
+
+supports      "centos"
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+# create unicorn service
+template "/etc/init.d/unicorn" do
+  owner "root"
+  group "root"
+  mode "0755"
+  source "unicorn.erb"
+  action :nothing
+end
